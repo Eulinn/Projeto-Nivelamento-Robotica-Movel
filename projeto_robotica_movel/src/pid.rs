230be0356caf71ad604fb6595e64eb2713307172
Euler Couto <87664037+Eulinn@users.noreply.github.com>
2024-08-16 09:31:29 -0300

    PID

--- projeto_robotica_movel/src/pid.rs
+++ projeto_robotica_movel/src/pid.rs
@@ -8,4 +8,38 @@ pub struct PID {
 }
 
 impl PID {
+
+    pub fn new(kp: f64, ki: f64, kd: f64) -> PID {
+        PID {
+            kp,
+            ki,
+            kd,
+            previous_error: 0.0,
+            integral: 0.0,
+        }
+    }
+
+    pub fn calculate(&mut self, setpoint: f64, measured_value: f64, delta_time: f64) -> f64 {
+        let error = setpoint - measured_value;
+        self.integral += error * delta_time;
+
+        self.integral = self.integral.clamp(-2.0,2.0); //limitação pra não ferrar com tudo
+
+
+
+
+
+        let derivative = (error - self.previous_error) / delta_time;
+
+        let derivative = derivative.clamp(-50.0, 50.0);
+
+
+        self.previous_error = error;
+
+
+        println!("Error: {}, Integral: {}, Derivative: {}, Output: {}, delta time: {}", error, self.integral, derivative, (self.kp * error) + (self.ki * self.integral) + (self.kd * derivative), delta_time);
+
+        (self.kp * error) + (self.ki * self.integral) + (self.kd * derivative)
+    }
+
 }
